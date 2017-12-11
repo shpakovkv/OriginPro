1130ed923d5fcd6e5eb8318b51ba918fc9a1d053
Konstantin Shpakov <konstantine.shpakov@gmail.com>
2017-12-11 20:02:22 +0300

    Added processing of cell types: string, int

--- LabTalk/LabTalk_SwapTwoRows.c
+++ LabTalk/LabTalk_SwapTwoRows.c
@@ -3,8 +3,9 @@ The script swaps two rows on all the Layers of the specified Book(s).
 
 How to use:
 1) Specify the Book(s) name(s).
-2) Specify the indexes of two rows to swap (NOTE: first row index is 1)
-3) Paste the script to the Command Window and press Enter.
+2) Specify the indexes of two rows to swap (NOTE: first row index is 1).
+3) Specify the cell type.
+4) Paste the script to the Command Window and press Enter.
 */
 StringArray BookNames;
 BookNames.Add("PeaksSeries1");
@@ -15,7 +16,22 @@ BookNames.Add("PeaksSeries4");
 int FirstRowToSwap = 4;
 int SecondRowToSwap = 5;
 
+// choose from "double", "int", "string" (default = "double")
+string cellType = "double"; 
+
 //--------------------------------------------------------
+switch (cellType){
+	case "int":
+		int first;
+		int second;
+	case "string":
+		string first$;
+		string second$;
+	default:
+		double first;
+		double second;
+}
+if
 for (iBook = 1; iBook<=BookNames.GetSize(); iBook++)
 {
 	window -a %(BookNames.GetAt(iBook)$);
@@ -26,12 +42,19 @@ for (iBook = 1; iBook<=BookNames.GetSize(); iBook++)
 		int ColNumber = wks.nCols;
 
 		for (iCol = 1; iCol <= ColNumber; iCol++)
-		{
-			double first = Cell(FirstRowToSwap, iCol);
-			double second = Cell(SecondRowToSwap, iCol);
-			Cell(FirstRowToSwap, iCol) = second;
-			Cell(SecondRowToSwap, iCol) = first;
+		{	
+			if (cellType != "string"){
+				first = Cell(FirstRowToSwap, iCol);
+				second = Cell(SecondRowToSwap, iCol);
+				Cell(FirstRowToSwap, iCol) = second;
+				Cell(SecondRowToSwap, iCol) = first;
+			}
+			else{
+				first$ = Cell(FirstRowToSwap, iCol)$;
+				second$ = Cell(SecondRowToSwap, iCol)$;
+				Cell(FirstRowToSwap, iCol)$ = second$;
+				Cell(SecondRowToSwap, iCol)$ = first$;
+			}
 		}
-		
 	}
 }

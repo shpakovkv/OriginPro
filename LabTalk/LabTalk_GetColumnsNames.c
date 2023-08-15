5a9bb5af54528c7422cea92475a2690bf1c4097e
Konstantin Shpakov <konstantine.shpakov@gmail.com>
2023-08-15 17:39:08 +0300

    Added new.

---
+++ LabTalk/LabTalk_GetColumnsNames.c
@@ -0,0 +1,14 @@
+/*
+The script prints the names of all columns located on the active layer of the active workbook.
+
+How to use:
+1) Select the target layer of the target WorkBook.
+2) Paste the script to the Command Window and press Enter.
+*/
+
+type "";
+for (int iCol = 1; iCol <= wks.nCols; iCol++)
+{
+	wks.col = iCol;
+	type "%(wks.col.name$)";
+}
